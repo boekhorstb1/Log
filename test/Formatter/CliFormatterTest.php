48bcf48d92eff8822a96635ea09c9ad3517ba4e0
Rafael te Boekhorst <boekhorst@b1-systems.de>
2021-11-23 10:30:43 +0000

    [WIP] Starting to make the unittests for the cli-formatter

---
+++ test/Formatter/CliFormatterTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Horde Log package
+ *
+ * This package is based on Zend_Log from the Zend Framework
+ * (http://framework.zend.com).  Both that package and this
+ * one were written by Mike Naberezny and Chuck Hagenbuch.
+ *
+ * @author   Rafael te Boekhorst <boekhorstb1@b1-systems.de>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Log
+ */
+namespace Horde\Log\Formatter\Test;
+use \PHPUnit\Framework\TestCase;
+
+use Horde\Log\Formatter\CliFormatter;
+
+class CliFormatterTest extends TestCase {
+
+
+}
