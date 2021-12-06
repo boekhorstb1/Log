3b16100cac769fa124d727e8c9d0e9f345a8ea2a
Rafael te Boekhorst <boekhorst@b1-systems.de>
2021-12-06 15:50:05 +0000

    first test for syslogger... missing some knowledge on workings/goals of this logger

--- test/Handler/SyslogHandlerTest.php
+++ test/Handler/SyslogHandlerTest.php
@@ -26,75 +26,23 @@ use Horde\Log\LogLevel;
 class SyslogHandlerTest extends TestCase
 {
     public function setUp(): void
-    { 
+    {
         date_default_timezone_set('America/New_York');
         $this->level1 = new LogLevel(Horde_Log::ALERT, 'Alert');
         $this->message1 = 'this is an emergency!';
         $this->logMessage1 = new LogMessage($this->level1, $this->message1, ['timestamp' => date('c')]);
+        $this->syshandler = new SyslogHandler();
     }
 
-    public function testConstructorThrowsWhenResourceIsNotStream()
+    public function testWrite(): void
     {
-        $this->expectException(LogException::class);
-        new SyslogHandler();
-        xml_parser_free($resource);
+        // dd($this->logMessage1);
+        $this->logMessage1->formatMessage([]);
+
+        $this->syshandler->setOption('ident', 'something');
+        $this->syshandler->setOption('openlogOptions', 1);
+
+
+        $this->assertTrue($this->syshandler->write($this->logMessage1));
     }
-
-    // public function testConstructorWithValidStream()
-    // {
-    //     $stream = fopen('php://memory', 'a');
-    //     $handler = new StreamHandler($stream);
-    //     $this->assertInstanceOf(LogHandler::class, $handler);
-    // }
-
-    // public function testConstructorWithValidUrl()
-    // {
-    //     $handler = new StreamHandler('php://memory');
-    //     $this->assertInstanceOf(LogHandler::class, $handler);
-    // }
-
-    // public function testConstructorThrowsWhenStreamCannotBeOpened()
-    // {
-    //     $this->expectException('Error');
-    //     new StreamHandler('');
-    // }
-
-    // public function testSettingBadOptionThrows()
-    // {
-    //     $this->expectException(LogException::class);
-    //     $handler = new StreamHandler('php://memory');
-    //     $handler->setOption('foo', 42);
-    // }
-
-    // public function testWrite() #See below comment: there is a small issue here
-    // {
-    //     $stream = fopen('php://memory', 'a');
-
-    //     $handler = new StreamHandler($stream);
-    //     $handler->log($this->logMessage1);
-
-    //     rewind($stream);
-    //     $contents = stream_get_contents($stream);
-    //     fclose($stream);
-
-    //     $message = $this->logMessage1->message();
-    //     $levelName = $this->logMessage1->level()->name();
-
-    //     $date = '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}-\d{2}:\d{2}';
-
-    //     $this->assertMatchesRegularExpression("/$date/", $contents);
-    //     $this->assertMatchesRegularExpression("/$message/", $contents);
-    //     // $this->assertMatchesRegularExpression("/$levelName/", $contents); // this does not match levelName, gives an error, because here levelName cannot reach to level->name with the format '%timestamp% %levelName%: %message%' . PHP_EOL... Need to fix default format for SimpleFormatter?
-    // }
-
-    // public function testWriteThrowsWhenStreamWriteFails()
-    // {
-    //     $this->expectException(LogException::class);
-    //     $stream = fopen('php://memory', 'a');
-    //     $handler = new StreamHandler($stream);
-    //     $handler->log($this->logMessage1);
-    //     fclose($stream);
-    //     $handler->write($this->logMessage1);
-    // }
-
 }
