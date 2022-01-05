e78dbe69ff618622847f7383db85231d3fc4e208
Rafael te Boekhorst <boekhorst@b1-systems.de>
2022-01-05 11:15:35 +0000

    updated and commented code to more readable version with local variables

--- test/LogLevelsTest.php
+++ test/LogLevelsTest.php
@@ -12,20 +12,11 @@ declare(strict_types=1);
 
 namespace Horde\Log\Test;
 
-use Horde\Util\HordeString;
-use Psr\Log\LogLevel as PsrLogLevel;
-
-
 use PHPUnit\Framework\TestCase;
-use Horde\Log\LogFilter;
-use Horde\Log\LogHandler;
-use Horde\Log\LogFormatter;
 use Horde\Log\LogMessage;
 use Horde\Log\LogLevel;
-use Horde\Log\LogException;
 use Psr\Log\LoggerInterface;
 use Horde\Log\LogLevels;
-use InvalidArgumentException;
 use Horde_Log;
 
 class LogLevelsTest extends TestCase
@@ -42,64 +33,81 @@ class LogLevelsTest extends TestCase
 
     public function testLogLevelsConstructorWithCustomLogLevel()
     {
-        $this->level24 = new LogLevel(24, 'Weirdness warning');
+        // Adding new LogLevelObject to the array $loadlevels[]
+        $level24 = new LogLevel(24, 'Weirdness warning');
+        $loadlevels[] = $level24;
 
-        $this->loadlevels[] = $this->level24;
-        $this->loadlevels[] = $this->level1;
-        $this->loglevels = new LogLevels($this->loadlevels);
-        $this->assertInstanceOf(LogLevels::class, $this->loglevels);
+        // Adding default LoglevelObject from setUp() to the array loadlevels[]
+        $loadlevels[] = $this->level1;
+
+        // Creating a LogLevels Object with the array loadlevels[]
+        $LogLevelsObject = new LogLevels($loadlevels);
+
+        // Testing if the Object $LogLevelsObject is created and is of Class LogLevels
+        $this->assertInstanceOf(LogLevels::class, $LogLevelsObject);
     }
 
     public function testLogLevelsRegisterFunctionWithGetByLevelName()
     {
-        $this->level35 = new LogLevel(35, 'Strangeness warning');
-        $this->loglevels->register($this->level35);
-        $this->assertEquals($this->loglevels->getByLevelName('strangeness warning'), $this->level35);
+        // Registering new LogLevelObject in default loglevels object
+        $level35 = new LogLevel(35, 'Strangeness warning');
+        $this->loglevels->register($level35);
+
+        // Asserting that returned LevelName is same as newly created Loglevel
+        $this->assertEquals($this->loglevels->getByLevelName('strangeness warning'), $level35);
     }
 
     public function testLogLevelsRegisterFunctionWithGetByCriticality()
     {
-        $this->level36 = new LogLevel(36, 'Absurdness warning');
-        $this->loglevels->register($this->level36);
-        $this->assertEquals($this->loglevels->getByLevelName('Absurdness warning'), $this->level36);
+        // Registering new LogLevelObject in default loglevels object
+        $level36 = new LogLevel(36, 'Absurdness warning');
+        $this->loglevels->register($level36);
+        $this->assertEquals($this->loglevels->getByCriticality(36), $level36);
     }
 
     public function testBothInitMethods()
     {
-        $aliaslevels = $this->loglevels->initWithAliasLevels();
+        $initWithAliasLevels = $this->loglevels->initWithAliasLevels();
         $initWithCanonicalLevels = $this->loglevels->initWithCanonicalLevels();
 
-        $loggerinterface_methods = get_class_methods(LoggerInterface::class);
-        $this->count = 0;
+        // the $levelNames array has the criticallity in the key and the names and aliases as value
+        $levelNamesCannonical = [
+            0 => 'emergency',
+            1 => 'alert',
+            2 => 'critical',
+            3 => 'error',
+            4 => 'warning',
+            5 => 'notice',
+            6 => 'info',
+            7 => 'debug'
+        ];
 
-        foreach ($loggerinterface_methods as $key => $interfacelevel) {
-            if ($interfacelevel !== 'log') {
-                $bynameCanonical = $initWithCanonicalLevels->getByLevelName($interfacelevel);
-                $bycriticalityCannonical = $initWithCanonicalLevels->getByCriticality($key);
-                $bynameAlias = $aliaslevels->getByLevelName($interfacelevel);
-                $bycriticalityAlias =  $aliaslevels->getByCriticality($key);
-                $this->assertEquals($bynameCanonical, $bycriticalityAlias);
-                $this->assertEquals($bynameAlias, $bycriticalityCannonical);
-                if ($aliaslevels->getByLevelName("emerg")->criticality() == $key) {
-                    $this->count += 1;
-                }
-                if ($aliaslevels->getByLevelName("crit")->criticality() == $key) {
-                    $this->count += 1;
-                }
-                if ($aliaslevels->getByLevelName("warn")->criticality() == $key) {
-                    $this->count += 1;
-                }
-                if ($aliaslevels->getByLevelName("err")->criticality() == $key) {
-                    $this->count += 1;
-                }
-                if ($aliaslevels->getByLevelName("information")->criticality() == $key) {
-                    $this->count += 1;
-                }
-                if ($aliaslevels->getByLevelName("informational")->criticality() == $key) {
-                    $this->count += 1;
-                }
+        $levelNamesAliases = [
+            0 => 'emerg',
+            2 => 'crit',
+            3 => 'err',
+            4 =>  'warn',
+            6 => 'information',
+            6 => 'informational'
+        ];
+
+        // testing cannonical names and levels
+        foreach ($levelNamesCannonical as $level => $names) {
+            $bynameCanonical = $initWithCanonicalLevels->getByLevelName($names);
+            $bycriticalityCannonical = $initWithCanonicalLevels->getByCriticality($level);
+            $this->assertEquals($bynameCanonical, $bycriticalityCannonical);
+        }
+
+        // testing alias names an levels
+        foreach ($levelNamesAliases as $level => $alias) {
+            $bynameAlias = $initWithAliasLevels->getByLevelName($alias);
+            $bycriticalityAlias =  $initWithAliasLevels->getByCriticality($level);
+
+            if ($bynameAlias->criticality() == 6) { // this if-else-statements are necessary because the aliases for info are longer than te cannonical names in case of info (level 6)
+                $this->assertStringContainsString($bycriticalityAlias->name(), $bynameAlias->name());
+            } else {
+                $this->assertStringContainsString($bynameAlias->name(), $bycriticalityAlias->name());
             }
         }
-        $this->assertEquals($this->count, 6); // initWithAliasLevels() has 6x more values than initWithCanonicalLevels()
     }
 }
